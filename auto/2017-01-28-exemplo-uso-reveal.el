8e11e0dfbfb290f22042bd6dccdf1f9baf9d1b68
Ney Lemke <neylemke@MacBook-Senne.local>
2018-01-16 11:10:05 -0200

    Tue Jan 16 11:10:05 -02 2018

---
+++ auto/2017-01-28-exemplo-uso-reveal.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "2017-01-28-exemplo-uso-reveal"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org3b475a4"
+    "sec:org2448c48"
+    "sec:org67cff91"
+    "sec:orga1d3243"
+    "sec:orgb404e8a"
+    "sec:orgefba962"
+    "sec:org4487018"
+    "sec:org55cec50"
+    "sec:orga292ab8"
+    "sec:org9c41ce5"
+    "sec:org0cf5575"
+    "sec:orgd0e3f7d"))
+ :latex)
+
